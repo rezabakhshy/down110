70c7cd321f1b22b8c048641e0d675f8e496fe896
rezabakhshy <87357982+rezabakhshy@users.noreply.github.com>
2022-01-18 12:33:59 +0330

    Update web.py

--- web.py
+++ web.py
@@ -4,7 +4,9 @@ app = Client("my_accound",api_id=13893053,api_hash="f586d92837b0f6eebcaa3e392397
 
 @app.on_message((filters.me) & filters.regex("!down "))
 def gif(client,message):
-    text=message.text
+    text=message.text    
+    message_id=message.message_id
+    chat_id=message.chat.id
     url=text[6:]
     response=requests.get(url)
     size=int(response.headers["content-length"])/1024/1024
@@ -13,10 +15,10 @@ def gif(client,message):
     m=url[x-4:]
     if m[0]!='.':
         m=url[x-5:]
+    client.edit_message_text(chat_id,message_id,"downloding...")
     with open(f"@REZABZ2{m}","wb") as f:
         f.write(response.content)
-    message_id=message.message_id
-    chat_id=message.chat.id
+    client.edit_message_text(chat_id,message_id,"downloding complate\n\nuploading now...")
     client.send_document(chat_id,f"@REZABZ2{m}",caption=f"\n**NAME:** @REZABZ2{m}\n**SAIZE:** {size} MB")
     os.remove(f"@REZABZ2{m}")
 
